94c954c20ed0f8ce40e190ec876da805829c3f47
fallxnstr <103467609+fallxnstr@users.noreply.github.com>
2022-04-24 21:31:42 -0500

    Update tweaking app.py

--- tweaking app.py
+++ tweaking app.py
@@ -1,18 +1,18 @@
-import subprocess
 import os
+import subprocess
+import winreg
 from kivy.app import App
+from kivy.config import Config
 from kivy.lang import Builder
-from kivy.uix.widget import Widget
+from kivy.properties import BooleanProperty, StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.config import Config
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import StringProperty, BooleanProperty
-import winreg
-
-Config.set('graphics', 'resizable', '0')
-Config.set('graphics', 'width', '800')
-Config.set('graphics', 'height', '600')
+from kivy.uix.widget import Widget
+Config.set("graphics", "resizable", "0")
+Config.set("graphics", "width", "800")
+Config.set("graphics", "height", "600")
 Config.write()
 
 
@@ -22,11 +22,24 @@ class WidgetEx(GridLayout):
         os.startfile("NSudoLG.exe")
 
     def on_powerplanbutton_click(self):
-        subprocess.call(["powercfg", "-import", "C:\Python\Tweaking App/Powerplan.pow", "11111111-1111-1111-1111-111111111111"])
-        subprocess.call(["powercfg", "-changename", "11111111-1111-1111-1111-111111111111","Powerplan", "Optimized for the best performance"])
-        subprocess.call(["powercfg", "-s", "11111111-1111-1111-1111-111111111111"])
-        subprocess.call(["powercfg", "-getactivescheme"])
+        subprocess.call(
+                        [
+                                "powercfg",
+                                "-import",
+                                "C:\Python\Tweaking App/Powerplan.pow",
+                                "11111111-1111-1111-1111-111111111111",
+                ]
+            )
 
+        subprocess.call(
+                    [
+                            "powercfg",
+                            "-changename",
+                            "11111111-1111-1111-1111-111111111111",
+                            "Powerplan",
+                            "Optimized for the best performance",
+            ]
+        )
 
 
 class TweakingApp(App):
